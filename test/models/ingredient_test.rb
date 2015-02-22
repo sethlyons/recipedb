d6225bdc0158a5b93e255d5088998cefdd69abfb
Seth Lyons <seth.lyons@gmail.com>
2015-02-22 15:22:40 -0500

    test initial ingredient model logic

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "it should be capitalize all ingredients regardless of input" do
+    d = Ingredient.new(name: "onion")
+    assert_equal("Onion", d.name)
+  end
+
+  test "it should not allow duplicate ingredients" do
+    d = Ingredient.new(name: "Onion")
+    e = Ingredient.new(name: "Onion")
+    d.save!
+    assert_raises(ActiveRecord::RecordInvalid) { e.save! }
+  end
 end
