6004398cc97552e0d98250dc5a2126550aae3e39
Seth Lyons <seth.lyons@gmail.com>
2015-02-22 15:22:23 -0500

    add initial logic to ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,10 @@
 class Ingredient < ActiveRecord::Base
+
+  # capitalize the name of each ingredient
+  def name=(val)
+    super(val.capitalize)
+  end
+
+  # ensure each ingredient is only stored once
+  validates_uniqueness_of :name
 end
